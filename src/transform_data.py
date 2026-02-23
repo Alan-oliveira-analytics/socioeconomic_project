8dfefa47db7eac24a5778dbc7313a9ebe9f69119
Alan Oliveira <alanoliveira@MacBook-Air-de-Alan.local>
2026-02-22 21:00:57 -0300

    transform data update - gdp_df

--- src/transform_data.py
+++ src/transform_data.py
@@ -20,5 +20,33 @@ df.head()
 country_name = df['series'][0][0]['pais']['nome']
 
 
+rows = []
+
 for item in df['series'][0]:
     print(item['pais'])
+    
+    for item in df['series'][0]:
+        pais = item.get('pais', {})
+        nome = pais.get('nome')
+        pais_id = pais.get('id')
+        serie = item.get('serie', [])
+
+        for entry in serie:
+            for key, value in entry.items():
+                year = key
+                gdp = value
+
+                rows.append({
+                    'country': nome,
+                    'country_id': pais_id,
+                    'year': year,
+                    'gdp': gdp
+                })
+
+gdp_df = pd.DataFrame(rows)
+
+gdp_df['year'] = pd.to_numeric(gdp_df['year'], errors='coerce').astype('Int64')
+gdp_df['gdp'] = pd.to_numeric(gdp_df['gdp'], errors='coerce')
+
+# remover linhas com valores nulos em 'gdp' ou 'year'
+gdp_df = gdp_df.dropna(subset=['gdp', 'year']).reset_index(drop=True)
